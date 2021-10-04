ccdb4be108f2dd672037142fe602a155cb3014b3
연훈 배 <sbaru1997@naver.com>
2021-10-04 19:52:38 +0900

    Connect the firestore to the story part

--- src/Routes/Story/StoryContainer.js
+++ src/Routes/Story/StoryContainer.js
@@ -1,10 +1,42 @@
 import React from "react";
 import StoryPresenter from "./StoryPresenter";
 
+import { dbService } from "fbase";
+
 export default class extends React.Component {
+    state = {
+        storyObj: null,
+        error: null,
+        loading: true
+    };
+
+    async componentDidMount() {
+        const {
+            match: {
+                params: { id }
+            },
+        } = this.props;
+        try {
+            const storyData = await dbService.doc(`story_box/${id}`).get();
+            const storyObj = storyData.data();
+            this.setState({
+                storyObj,
+            });
+        }
+        catch {
+            this.setState({ error: "Can't find anything." })
+        } finally {
+            this.setState({ loading: false });
+        }
+    }
     render() {
+        const { storyObj, error, loading } = this.state;
         return (
-            <StoryPresenter />
+            <StoryPresenter
+                error={error}
+                loading={loading}
+                storyObj={storyObj}
+            />
         )
     }
 }
--- src/Routes/Story/StoryPresenter.js
+++ src/Routes/Story/StoryPresenter.js
@@ -2,12 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import Helmet from "react-helmet";
 
-const StoryPresenter = () => (
-    <>
-        <Helmet>
-            <title>Story | BYHOON</title>
-        </Helmet>
-    </>
-);
+const StoryPresenter = ({ error, loading, storyObj }) => {
+    console.log(storyObj);
+    return (
+        <>
+            <Helmet>
+                <title>Story | BYHOON</title>
+            </Helmet>
+        </>
+    )
+};
 
 export default StoryPresenter;
