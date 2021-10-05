bc1aeb91daedd6e4c55445bfe5df575e2b6e896e
연훈 배 <sbaru1997@naver.com>
2021-10-06 03:17:28 +0900

    Add the gallery part to display the entire image.

--- src/Routes/Gallery/GalleryPresenter.js
+++ src/Routes/Gallery/GalleryPresenter.js
@@ -5,15 +5,72 @@ import Helmet from "react-helmet";
 
 import { dbService } from "fbase";
 
+const ImageContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-top: 16px;
+`;
+
+const Imagebox = styled.div`
+    display: inline-flex;
+    border-radius: 2;
+    border: 1px solid #eaeaea;
+    margin-bottom: 8px;
+    margin-right: 8px;
+    width: 200px;
+    height: 200px;
+    padding: 4px;
+    box-sizing: border-box;
+`;
+
+const Img = styled.img`
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+`;
 
 const GalleryPresenter = () => {
+    const history = useHistory();
+    const isMounted = useRef(false);
 
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        isMounted.current = true;
+
+        dbService.collection("story_box").onSnapshot((snapshot) => {
+            const boxArray = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            boxArray.forEach(box => {
+                box.mainImages[0].attachmentArray.forEach((attachment, index) => {
+                    const imageObj = {
+                        storyId: box.id,
+                        imgId: box.mainImages[0].imageId[index],
+                        attachmentArray: attachment,
+                    }
+                    setImages((prev) => [imageObj, ...prev]);
+                })
+            });
+        });
+
+        return () => (isMounted.current = false);
+    }, []);
     return (
         <>
             <Helmet>
                 <title>Gallery | BYHOON</title>
             </Helmet>
-
+            <ImageContainer>
+                {images.length > 0 ?
+                    (images.map((img) => (
+                        <Imagebox key={img.imgId}>
+                            <Img src={img.attachmentArray} />
+                        </Imagebox>
+                    ))) : null}
+            </ImageContainer>
         </>
 
     )
