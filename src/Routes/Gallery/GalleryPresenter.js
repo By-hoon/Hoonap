d653d8e5acd68573dd68458629d54435effb66c0
연훈 배 <sbaru1997@naver.com>
2021-11-02 18:06:33 +0900

    style: 갤러리 스타일 추가

--- src/Routes/Gallery/GalleryPresenter.js
+++ src/Routes/Gallery/GalleryPresenter.js
@@ -9,81 +9,45 @@ import { dbService } from "fbase";
 const FlexContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
+    justify-content: space-around;
 `;
 
-const FigContainer = styled.figure`
-    position: relative;
-    display: inline-block;
-    margin: 20px;
-    max-width: 250px;
+const ImageContainer = styled.div`
+    margin: 40px;
+    width: 250px;
     height: 250px;
-    width: 100%;
-    color: #bbb;
-    font-size: 16px;
-    box-shadow: none !important;
-    transform: translateZ(0);
-
-    *,&:before, &:after{
-        box-sizing: border-box;
-        transition: all 0.2s linear;
-    }
-
-    &:hover{
-        figcaption{
-            opacity: 1;
-        }
-    }
-`;
-
-const Img = styled.img`
-  max-width: 100%;
-  height: 100%;
-  object-fit: contain;
-  backface-visibility: hidden;
-  /* vertical-align: top; */
-  padding: 10px;
-`;
-
-const FigCaption = styled.figcaption`
-  position: absolute;
-  top: 5px;
-  bottom: 5px;
-  left: 5px;
-  right: 5px;
-  opacity: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  /* border-radius: 50%; */
-
-`;
-
-const SpanBox = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    position: absolute;
-    top: 90%;
-    left: 50%;
-    transform: translate(-50%, -55%);
-    z-index: 1;
-    padding: 10px;
-`;
-
-const PreviewSpan = styled.span`
-    font-size: 13px;
-    color: #dfe6e9;
-    &:hover{
-            color: #6c5ce7;
+    border-radius: 10px;
+    display: block;
+    position: relative;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-image: url(${(props) => props.imgUrl}); 
+    :after {
+        transform: scale(0.95) translateY(36px) translateZ(-30px);
+        filter: blur(20px);
+        opacity: 0.9;
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-image: inherit; 
+        background-size: contain;
+        z-index: -1;
+        transition: filter .3s ease;
     }
 `;
+
+// const LightContainer = styled.div`
+//     background: radial-gradient(ellipse at center, rgba(255,255,255,0.3) 0%,rgba(255,255,255,0) 60%);
+//     background-blend-mode: lighten;
+//     position: fixed;
+//     pointer-events: none;
+// `;
 
 const ImageLink = styled(Link)``;
 
-
-//      TODO: story id받아와서 ImageLInk에 넣어주기.
-//            imageId CommentLink에 넣어주기.
-
 const GalleryPresenter = () => {
-    const history = useHistory();
     const isMounted = useRef(false);
 
     const [images, setImages] = useState([]);
@@ -91,20 +55,13 @@ const GalleryPresenter = () => {
     useEffect(() => {
         isMounted.current = true;
 
-        dbService.collection("story_box").onSnapshot((snapshot) => {
-            const boxArray = snapshot.docs.map((doc) => ({
+        dbService.collection("image_box").onSnapshot((snapshot) => {
+            const imageArray = snapshot.docs.map((doc) => ({
                 id: doc.id,
                 ...doc.data(),
             }));
-            boxArray.forEach(box => {
-                box.mainImages.attachmentArray.forEach((attachment, index) => {
-                    const imageObj = {
-                        storyId: box.id,
-                        imgId: box.mainImages.imageId[index],
-                        attachmentArray: attachment,
-                    }
-                    setImages((prev) => [imageObj, ...prev]);
-                })
+            imageArray.forEach(image => {
+                setImages((prev) => [image, ...prev]);
             });
         });
 
@@ -118,17 +75,10 @@ const GalleryPresenter = () => {
             <FlexContainer>
                 {images.length > 0 ?
                     (images.map((img) => (
-                        <FigContainer key={img.imgId}>
-                            <ImageLink to={`/comment/${img.imgId}`}>
-                                <Img src={img.attachmentArray} />
-                                <FigCaption>
-                                    <SpanBox>
-                                        <PreviewSpan>3 likes</PreviewSpan>
-                                        <PreviewSpan>5 comments</PreviewSpan>
-                                    </SpanBox>
-                                </FigCaption>
-                            </ImageLink>
-                        </FigContainer>
+                        <ImageLink key={img.id} to={`/comment/${img.id}`}>
+                            <ImageContainer imgUrl={img.attachmentId} />
+                            {/* <LightContainer /> */}
+                        </ImageLink>
                     ))) : null}
             </FlexContainer>
         </>
